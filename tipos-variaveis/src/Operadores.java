be48b1939e9d0f29962fe21f1b9cab11713273e1
Pedro Bigas <pedrobbigas@gmail.com>
2024-12-11 23:43:06 -0300

    add operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,45 @@
+public class Operadores {
+    public static void main(String[] args) {
+        double soma = 10.5 + 15.7;
+        int subtracao = 113 - 25;
+        int multiplicacao = 20 * 7;
+        int divisao = 15 / 3;
+        int modulo = 18 % 3;
+        double resultado = (20 * 7) + (20 / 4);
+
+        System.out.println(modulo);
+
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        /*
+         * EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+         * if(a==b)
+         * resultado = "verdadeiro";
+         * else
+         * resultado = "falso";
+         */
+
+        // MESMA CONDICIONAL, MAS DESSA VEZ, UTILIZANDO O OPERADOR CONDICIONAL TERNÁRIO
+        String resultado = (a == b) ? "verdadeiro" : "false";
+
+        System.out.println(valor);
+
+        // ComparacaoAvancada.java
+        int numero1 = 1;
+        int numero2 = 1;
+
+        if (numero1 == 2 & numero2++ == 2)
+            System.out.println("As 2 condições são verdadeiras");
+
+        System.out.println("O numero 1 agora é " + numero1);
+        System.out.println("O numero 2 agora é " + numero2);
+
+        // Vamos mudar a linha 5 do código acima para: if(numero1== 2 && numero2 ++ == 2
+        // )
+
+    }
+
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        final float pi = 3.146f;
+        final double e;
+
+        e = 1.234e2;
+
+        String nome = "Pedro Bias";
+        String dataAniversario = "25/10/2002";
+        int lote = 35456;
+        boolean aprovado;
+
+        aprovado = true;
+
+        System.out.println(pi); // vai imprimir "3.146f"
+        System.out.println(e);// vai imprimir "1.234e2"
+        System.out.println(nome);// vai imprimir "Estevao Dias"
+        System.out.println(dataAniversario);// vai imprimir "23/05/2000"
+        System.out.println(lote);// vai imprimir "35456"
+        System.out.println(aprovado);// vai imprimir "true"
     }
 }
